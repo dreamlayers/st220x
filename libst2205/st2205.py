0c1f21d67487f4ada1d3c7600c6317c8ef2ccb2b
Boris Gjenero <boris.gjenero@gmail.com>
2015-11-24 14:35:05 -0500

    Add Python interface for libst2205, with Pillow PIL support

---
+++ libst2205/st2205.py
@@ -0,0 +1,68 @@
+from ctypes import *
+from PIL import Image
+
+# Handle definition for the st2205_* routines
+class st2205_handle(Structure):
+    _fields_ = [("fd", c_int),
+                ("width", c_uint),
+                ("height", c_uint),
+                ("bpp", c_int),
+                ("proto", c_int),
+                ("buff", POINTER(c_char)),
+                ("oldpix", POINTER(c_ubyte)),
+                ("offx", c_int),
+                ("offy", c_int)
+               ]
+
+l = cdll.LoadLibrary("libst2205.so.2")
+
+st2205_open = CFUNCTYPE(POINTER(st2205_handle), c_char_p) \
+              (("st2205_open", l))
+
+st2205_close = CFUNCTYPE(None, POINTER(st2205_handle)) \
+               (("st2205_close", l))
+
+st2205_send_data = CFUNCTYPE(None, POINTER(st2205_handle), POINTER(c_ubyte)) \
+                   (("st2205_send_data", l))
+
+st2205_send_partial = CFUNCTYPE(None, POINTER(st2205_handle), POINTER(c_ubyte),
+                                c_int, c_int, c_int, c_int) \
+                      (("st2205_send_partial", l))
+
+st2205_backlight = CFUNCTYPE(None, POINTER(st2205_handle), c_int) \
+                   (("st2205_backlight", l))
+
+st2205_lcd_sleep = CFUNCTYPE(None, POINTER(st2205_handle), c_int) \
+                   (("st2205_lcd_sleep", l))
+
+class ST2205:
+    def __init__(self, dev = '/dev/disk/by-id/usb-SITRONIX_MULTIMEDIA-0:0'):
+        self.h = st2205_open(dev)
+        self.i = None
+        assert self.h
+
+    def close(self):
+        st2205_close(self.h)
+        self.h = None
+
+    def backlight(self, on):
+        st2205_backlight(self.h, 1 if on else 0)
+
+    def lcd_sleep(self, sleep):
+        st2205_lcd_sleep(self.h, 1 if sleep else 0)
+
+    def get_image(self):
+        if self.i is None:
+            self.i = Image.new("RGB",
+                               (self.h.contents.width, self.h.contents.height),
+                               "black")
+        return self.i
+
+    def update(self):
+        st2205_send_data(self.h, cast(c_char_p(self.i.tobytes()),
+                                      POINTER(c_ubyte)))
+
+    def update_part(self, xs, ys, xe, ye):
+        st2205_send_partial(self.h, cast(c_char_p(self.i.tobytes()),
+                                         POINTER(c_ubyte)),
+                            xs, ys, xe, ye)
